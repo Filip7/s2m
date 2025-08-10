f1ab2f4a2449ecadf99400b824cacd38e44cb123
Filip M <filipm7@protonmail.com>
2025-08-10 23:26:25 +0200

    feat: reduce regex usage

--- main.go
+++ main.go
@@ -17,57 +17,70 @@ func removeEmptyStrings(s []string) []string {
 	return r
 }
 
+func addCorrectEnding(i int, lines []string, isFirst int) {
+	if i != len(lines)-1 {
+		lines[i] += ","
+	} else {
+		lines[i] += ";"
+	}
+
+	// if new insert block, replace prev ',' with ';'
+	if isFirst == 0 && i != 0 {
+		lines[i-1] = strings.TrimRight(lines[i-1], ",")
+		lines[i-1] += ";"
+	}
+}
+
 func convertSingleLineToMultilineSQL(input string) string {
 	lines := strings.Split(input, ";")
 	lines = removeEmptyStrings(lines)
 
-	// (?i) -> case insensitive
-	// w+   -> match word
-	// (.*) -> match everything inside '(' and ')'
-	r := regexp.MustCompile(`(?i)insert into \w+(.*) values `)
-	match := r.FindAllString(lines[0], -1)
-	var match1 string
-	if match == nil {
-		match1 = ""
-	} else {
-		match1 = match[0]
-	}
+	// (?i)  -> case insensitive
+	// (\w+) -> match word
+	// (.*)  -> match everything inside '(' and ')'
+	r := regexp.MustCompile(`(?i)insert into (\w+)(.*) values `)
 
 	isFirst := 0
+	match, table := "", ""
 	for i := range len(lines) {
-		match = r.FindAllString(lines[i], -1)
-		var match2 string
-		if match != nil {
-			match2 = match[0]
+		firstWord := fmt.Sprintf("%.7s", lines[i])
+		if strings.Contains(strings.ToLower(firstWord), "insert") {
+			// is an insert
+			if isFirst == 0 {
+				insertMatch := r.FindStringSubmatch(lines[i])
+				match = insertMatch[0]
+				table = insertMatch[1]
+			}
+			// get first 4 words, and get the table name from it
+			splitLine := strings.SplitN(lines[i], " ", 5)
+			locatedTableName := splitLine[len(splitLine)-3]
+			if locatedTableName != table {
+				// if new table insert has been found
+				isFirst = 0
+				addCorrectEnding(i, lines, isFirst)
+				insertMatch := r.FindStringSubmatch(lines[i])
+				match = insertMatch[0]
+				table = locatedTableName
+				isFirst = 1
+				continue
+			}
 		} else {
-			match2 = ""
-		}
-
-		if match1 != match2 {
+			// is not an insert
 			isFirst = 0
+			addCorrectEnding(i, lines, isFirst)
+			continue
 		}
 
-		if i != len(lines)-1 {
-			lines[i] += ","
-		} else {
-			lines[i] += ";"
-		}
-
-		// if new insert block, replace prev ',' with ';'
-		if isFirst == 0 && i != 0 {
-			lines[i-1] = strings.TrimRight(lines[i-1], ",")
-			lines[i-1] += ";"
-		}
+		addCorrectEnding(i, lines, isFirst)
 
 		if isFirst != 0 {
 			// remove the insert part and replace it with nothing
-			lines[i] = strings.Replace(lines[i], match2, "", 1)
+			lines[i] = strings.Replace(lines[i], match, "", 1)
 			// remove the starting newline char that we got at the begining by spliting at ;
 			lines[i] = strings.Replace(lines[i], "\n", "", 1)
 			// return newline char and add spaces for the len of insert part, to pretify the output
-			lines[i] = "\n" + strings.Repeat(" ", len(match2)) + lines[i]
+			lines[i] = "\n" + strings.Repeat(" ", len(match)) + lines[i]
 		}
-		match1 = match2
 		isFirst += 1
 	}
 
