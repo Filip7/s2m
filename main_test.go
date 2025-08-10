110be33615130545c9460cbd52551b0b570f60d7
Filip M <filipm7@protonmail.com>
2025-08-10 12:59:12 +0200

    feat: better output format

--- main_test.go
+++ main_test.go
@@ -4,36 +4,26 @@ import (
 	"testing"
 )
 
-var input = `INSERT INTO films (code, title, did, date_prod, kind) VALUES ('B6717', 'Tampopo', 110, '1985-02-10', 'Comedy');
-	INSERT INTO films (code, title, did, date_prod, kind) VALUES ('HG120', 'The Dinner Game', 140, DEFAULT, 'Comedy');`
-
-var output = `INSERT INTO films (code, title, did, date_prod, kind) VALUES ('B6717', 'Tampopo', 110, '1985-02-10', 'Comedy'),
-	('HG120', 'The Dinner Game', 140, DEFAULT, 'Comedy');`
-
-func TestMain(t *testing.T) {
-	out := convertSingleLineToMultilineSQL(input)
-
-	if out != output {
-		t.Errorf("Expected\n\"%s\"\ngot\n\"%s\"", output, out)
+func Test_convertSingleLineToMultilineSQL(t *testing.T) {
+	tests := []struct {
+		name string // description of this test case
+		// Named input parameters for target function.
+		input string
+		want  string
+	}{
+		{"First use case",
+`INSERT INTO films (code, title, did, date_prod, kind) VALUES ('B6717', 'Tampopo', 110, '1985-02-10', 'Comedy');
+INSERT INTO films (code, title, did, date_prod, kind) VALUES ('HG120', 'The Dinner Game', 140, DEFAULT, 'Comedy');`,
+`INSERT INTO films (code, title, did, date_prod, kind) VALUES ('B6717', 'Tampopo', 110, '1985-02-10', 'Comedy'),
+                                                             ('HG120', 'The Dinner Game', 140, DEFAULT, 'Comedy');`,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := convertSingleLineToMultilineSQL(tt.input)
+			if got != tt.want {
+				t.Errorf("Expected\n\"%s\"\ngot\n\"%s\"", tt.want, got)
+			}
+		})
 	}
 }
-
-// func Test_convertSingleLineToMultilineSQL(t *testing.T) {
-// 	tests := []struct {
-// 		name string // description of this test case
-// 		// Named input parameters for target function.
-// 		input string
-// 		want  string
-// 	}{
-// 		// TODO: Add test cases.
-// 	}
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			got := convertSingleLineToMultilineSQL(tt.input)
-// 			// TODO: update the condition below to compare got with tt.want.
-// 			if true {
-// 				t.Errorf("convertSingleLineToMultilineSQL() = %v, want %v", got, tt.want)
-// 			}
-// 		})
-// 	}
-// }
